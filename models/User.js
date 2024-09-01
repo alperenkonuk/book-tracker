bef67c1fdcd9514140c9cf9ad752734b93e2a1c6
alperen <alperenkonuk93@gmail.com>
2024-09-01 23:59:41 +0300

    Delete unused methods in user model

--- models/User.js
+++ models/User.js
@@ -10,51 +10,22 @@ const User = {
     }
   },
 
-  async checkByUserName(username) {
+  async checkByUserName(name) {
     try {
-      return await db.query('SELECT * FROM users WHERE name=$1', [username]);
+      return await db.query('SELECT * FROM users WHERE name=$1', [name]);
     } catch (e) {
       console.error('User does not exist: ', e.stack);
       return false;
     }
   },
 
-  async registerUser(username, email, password, publicProfile) {
+  async registerUser(name, email, password, publicProfile) {
     try {
-      return await db.query("INSERT INTO users (name,email,password,public) VALUES($1,$2,$3,$4) RETURNING id", [username, email, password, publicProfile]);
+      return await db.query("INSERT INTO users (name,email,password,public) VALUES($1,$2,$3,$4) RETURNING id", [name, email, password, publicProfile]);
     } catch (e) {
       console.log('Error registering user: ', e.stack);
     }
   },
-
-  async isValidUser(email, password) {
-    try {
-      await db.query('SELECT * FROM users WHERE email=$1 and password=$2', [email, password]);
-      return true;
-    } catch (e) {
-      console.error('User does not exist: ', e.stack);
-      return false;
-    }
-  },
-
-  async getUserId(email) {
-    try {
-      const res = await db.query('SELECT id FROM users WHERE email=$1', [email]);
-      console.log(res.rows[0].id)
-      return res.rows[0].id
-    } catch (e) {
-      console.error('Error getting user id: ', e.stack);
-    }
-  },
-  async getUserName(email) {
-    try {
-      const res = await db.query('SELECT name FROM users WHERE email=$1', [email]);
-      console.log(res.rows[0].name)
-      return res.rows[0].name
-    } catch (e) {
-      console.error('Error getting user id: ', e.stack);
-    }
-  }
 }
 
 export default User;
